d0c73035ab34df8dbf9878acba2a342e38f3a718
Daniel Waite <danielwaite6@hotmail.com>
2021-09-27 23:00:28 -0300

    oi

--- test/app_bar_controller_test.dart
+++ test/app_bar_controller_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mobx/mobx.dart' as mobx;
 import 'package:mocktail/mocktail.dart';
 import 'package:split_it/modules/home/models/dashboard_model.dart';
 import 'package:split_it/modules/home/widgets/app_bar/app_bar_controller.dart';
@@ -20,15 +21,20 @@ void main() {
     expect(controller.state, isInstanceOf<AppBarStateEmpty>());
 
     final states = <AppBarState>[];
-    controller.listen((state) => states.add(state));
-    when(repository.getDashboard)
-        .thenAnswer((_) async => DashboardModel(send: 100, receive: 50));
+    mobx.autorun((_) {
+      states.add(controller.state);
+    });
+
+    when(repository.getDashboard).thenAnswer(
+      (_) async => DashboardModel(send: 100, receive: 50),
+    );
 
     await controller.getDashboard();
 
-    expect(states[0], isInstanceOf<AppBarStateLoading>());
-    expect(states[1], isInstanceOf<AppBarStateSuccess>());
-    expect(states.length, 2);
+    expect(states[0], isInstanceOf<AppBarStateEmpty>());
+    expect(states[1], isInstanceOf<AppBarStateLoading>());
+    expect(states[2], isInstanceOf<AppBarStateSuccess>());
+    expect(states.length, 3);
   });
 
   test("Testando o GetDashboard - Failure", () async {
@@ -36,12 +42,15 @@ void main() {
     expect(controller.state, isInstanceOf<AppBarStateEmpty>());
 
     final states = <AppBarState>[];
-    controller.listen((state) => states.add(state));
+    mobx.autorun((_) {
+      states.add(controller.state);
+    });
+
     when(repository.getDashboard).thenThrow("Deu erro..");
 
     await controller.getDashboard();
 
-    expect(states[0], isInstanceOf<AppBarStateLoading>());
+    expect(states[0], isInstanceOf<AppBarStateEmpty>());
     expect(states[1], isInstanceOf<AppBarStateFailure>());
     expect((states[1] as AppBarStateFailure).message, "Deu erro..");
     expect(states.length, 2);
